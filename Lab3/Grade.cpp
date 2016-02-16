78d18a784ba2cfd7c22091b0195e51c3ec7af296
C-Timm <christopher.timm@student.dwc.edu>
2016-02-16 14:23:53 -0500

    Update Grade.cpp

--- Lab3/Grade.cpp
+++ Lab3/Grade.cpp
@@ -12,23 +12,27 @@ Grade::Grade()
 
 void Grade::Q1(int quiz1)
 {
-	quiz1 * 0.125;
+	int q1 = quiz1;
+	quiz1 = q1 * 0.125;
 }
 
 void Grade::Q2(int quiz2)
 {
-	quiz2 * 0.125;
+	int q2 = quiz2;
+	quiz2 = q2 * 0.125;
 }
 
 void Grade::mid(int midterm)
 {
-	midterm * 0.25;
+	int m = midterm;
+	midterm = m * 0.25;
 }
 
 void Grade::fin(int finalexam)
 {
-	finalexam * 0.50;
-	
+	int f = finalexam;
+	finalexam = finalexam * 0.50;
+
 }
 
 int Grade::get_Q1()
@@ -36,7 +40,7 @@ int Grade::get_Q1()
 	return this->quiz1;
 }
 
-int Grade::get_Q2() 
+int Grade::get_Q2()
 {
 	return this->quiz2;
 }
