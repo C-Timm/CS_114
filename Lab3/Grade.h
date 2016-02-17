95d6b42ce0c277162835be8e1d2fdc882c350105
C-Timm <christopher.timm@student.dwc.edu>
2016-02-16 22:29:10 -0500

    Update Grade.h

--- Lab3/Grade.h
+++ Lab3/Grade.h
@@ -1,22 +1,67 @@
+/*
+ * Grade.h
+ *
+ *  Created on: Feb 16, 2016
+ *      Author: Bigtimetimmmy
+ */
+
 #pragma once
+#include <string>
+#include <iostream>
 
 class Grade
 {
-public:
 	Grade();
-	void Q1(int quiz1);
-	void Q2(int quiz2);
-	void mid(int midterm);
-	void fin(int finalexam);
-	int get_Q1();
-	int get_Q2();
-	int get_Midterm();
-	int get_Finalexam();
-	
-private:
-	int quiz1;
-	int quiz2;
-	int midterm;
-	int finalexam;
-};
 
+	double quiz1 = 0;
+	double quiz2 = 0;
+	double midterm = 0;
+	double finalexam = 0;
+	double finalGrade = 0;
+
+
+	void calcGrade()
+	{
+		double qTotal;
+		double mTotal;
+		double fTotal;
+
+		qTotal = quiz1 + quiz2;
+		qTotal /= 20;
+		qTotal *= 0.25;
+
+		mTotal = midterm;
+		mTotal /= 100;
+		mTotal *= 0.25;
+
+		fTotal = finalexam;
+		fTotal /= 100;
+		fTotal *= 0.50;
+
+		finalGrade = fTotal + mTotal + qTotal;
+
+	}
+
+public:
+
+
+	double getGrade()
+	{
+	calcGrade();
+	return finalGrade;
+	}
+
+	void inputGrade(double q1, double q2, double mid, double fin )
+	{
+		quiz1 = q1;
+		quiz2 = q2;
+		midterm = mid;
+		finalexam = fin;
+
+
+	}
+
+
+
+
+};
