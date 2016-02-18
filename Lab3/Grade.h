53dc8f8debec9f80ca887fe80fa1ff9af305ca0b
C-Timm <christopher.timm@student.dwc.edu>
2016-02-18 13:24:47 -0500

    Update Grade.h

--- Lab3/Grade.h
+++ Lab3/Grade.h
@@ -1,65 +1,29 @@
 /*
- * Grade.cpp
+ * Grade.h
  *  Created on: Feb 17, 2016
  *      Author: C-Timm
  */
 
-#include "Grade.h"
-#include <iostream>
-#include <iomanip>
+#ifndef Grade_h_
+#define Grade_h_
+#include <math.h>
+#include "StudentRecord.h"
 
+using namespace std;
 
-Grade::Grade(StudentRecord* rec) {
-	memcpy(&student, rec, sizeof(StudentRecord));
-	percent();
-
-}
-
-Grade::~Grade()
+class Grade
 {
-}
-void Grade::percent()
-{
-	//quizzes
-	gradePercent = (float)((((student.quiz1 + student.quiz2)/ 2.0f) /10.0f)*0.25f);
-	//Midterm
-	gradePercent += (float)((student.midterm /100.0f)*0.25f);
-	//Final
-	gradePercent += (float)((student.finalexam / 100.0f)*0.50f);
+public:
+	Grade(StudentRecord* rec);
+	~Grade();
+	void Letter();
+	float getGrade();
+	void percent();
+private:
+	StudentRecord student;
+	float gradePercent;
+	char gradeLetter;
+};
+#endif
 
-	gradePercent *= 100.0f;
-
-}
-
-float Grade::getGrade()
-{
-	return gradePercent;
-}
-
-void Grade::Letter()
-{
-	if (gradePercent >= 90.0f)
-	{
-		gradeLetter = 'A';
-		cout << "Final Grade: " << gradeLetter << endl;
-	}
-	else if (gradePercent >= 80.0f){
-		gradeLetter = 'B';
-		cout << "Final Grade: " << gradeLetter << endl;
-	}
-	else if (gradePercent >= 70.0f){
-		gradeLetter = 'C';
-		cout << "Final Grade: " << gradeLetter << endl;
-	}
-	else if (gradePercent >= 60.0f){
-		gradeLetter = 'D';
-		cout << "Final Grade: " << gradeLetter << endl;
-	}
-	else
-	{
-		gradeLetter = 'F';
-		cout << "Final Grade: " << gradeLetter << endl;
-	}
-
-}
 
